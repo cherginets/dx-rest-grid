6332445b875499f10e26d70f84bdbddcc20c124b
anton cherginets <anton.cherginets@gmail.com>
2022-03-24 15:31:54 +0300

    init stable state

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,62 @@
 import React from 'react'
 
-import { ExampleComponent } from 'dx-rest-grid'
+import DxRestGrid from 'dx-rest-grid'
 import 'dx-rest-grid/dist/index.css'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from './theme'
+import { Container, Paper, Typography } from '@mui/material'
+
+type Row = {
+  id: number
+  name: string
+  email: string
+  balance: number
+}
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ThemeProvider theme={theme}>
+    <Container>
+      <Typography variant={'h4'} style={{marginBottom: 20,}}>Grid when all features enabled</Typography>
+      <Paper>
+        <DxRestGrid<Row>
+          id={'EXAMPLE_TABLE'}
+          fetchAction={(params) => {
+            return fetchUsers({
+              currentPage: Math.floor(params.offset / params.limit) + 1,
+              pageSize: params.limit + 1,
+            })
+          }}
+        />
+      </Paper>
+    </Container>
+  </ThemeProvider>
 }
 
 export default App
+
+// region Temporary API
+
+const generatedData: Row[] = new Array(444).fill(null).map((i) => {
+  const id = parseInt(i) + 1;
+  return {
+    id,
+    balance: Math.floor(Math.random() * 100000),
+    email: `test${id}@test.domain`,
+    name: `User N-${id}`
+  }
+});
+
+const fetchUsers = ({ currentPage, pageSize }: {
+  currentPage: number
+  pageSize: number
+}) => {
+  const resultRows = [...generatedData];
+
+  const total = resultRows.length;
+  const offset = (currentPage - 1) * pageSize;
+  const rows = generatedData.slice(offset, offset + pageSize);
+
+  return Promise.resolve({ rows, total, })
+}
+
+// endregion
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,45 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import React, { useState } from 'react'
+import { PagingState, IntegratedPaging } from '@devexpress/dx-react-grid'
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  PagingPanel
+} from '@devexpress/dx-react-grid-material-ui'
 
-interface Props {
-  text: string
+export type DxRestGridProps<T> = {
+  id: string
+  fetchAction: (params: { offset: number; limit: number }) => Promise<{
+    rows: ReadonlyArray<T>
+    total: number
+  }>
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const DxRestGrid = <T,>({
+  id,
+  fetchAction
+}: DxRestGridProps<T>): JSX.Element => {
+  console.log('id', id);
+  console.log('fetchAction', fetchAction);
+  const [columns] = useState([
+    { name: 'name', title: 'Name' },
+    { name: 'gender', title: 'Gender' },
+    { name: 'city', title: 'City' },
+    { name: 'car', title: 'Car' }
+  ])
+  const [rows] = useState([{ name: '123' }])
+
+  return (
+    <div>
+      <Grid rows={rows} columns={columns}>
+        <PagingState defaultCurrentPage={0} pageSize={5} />
+        <IntegratedPaging />
+        <Table />
+        <TableHeaderRow />
+        <PagingPanel />
+      </Grid>
+    </div>
+  )
 }
+
+export default DxRestGrid
